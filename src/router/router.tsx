dc1ee5ca2f68af43875f73e13c604b16c786319b
ZRunner <z.runner.mc@gmail.com>
2023-07-10 22:53:56 -0400

    feat(doc): add proper redirection

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,6 +5,11 @@ import ErrorPage from "../pages/ErrorPage";
 import Home from "../pages/Home";
 import PublicLayout from "../pages/layouts/PublicLayout";
 
+const Documentation = () => {
+  window.location.replace("https:/axobot.rtfd.io");
+  return null;
+};
+
 const router = createBrowserRouter([
   {
     element: <PublicLayout />,
@@ -17,6 +22,10 @@ const router = createBrowserRouter([
       {
         path: "/dashboard",
         element: <Dashboard />,
+      },
+      {
+        path: "/documentation",
+        element: <Documentation />,
       },
     ],
   },
