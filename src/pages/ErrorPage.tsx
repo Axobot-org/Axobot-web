974f324987bf4ccd2c03f48123dbbd6a294fdb43
ZRunner <z.runner.mc@gmail.com>
2023-07-10 22:17:04 -0400

    feat(core): add pages components

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,37 @@
+import { Stack, Typography } from "@mui/material";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+import Appbar from "../components/common/Appbar";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+
+  let errorMessage: string;
+
+  if (isRouteErrorResponse(error)) {
+    // error is type `ErrorResponse`
+    errorMessage = error.error?.message || error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === "string") {
+    errorMessage = error;
+  } else {
+    console.error(error);
+    errorMessage = "Unknown error";
+  }
+
+  return (
+    <Stack
+      alignItems="center"
+    >
+      <Appbar />
+      <h1>Oops!</h1>
+      <Typography my={1}>
+        Sorry, an unexpected error has occurred
+      </Typography>
+      <Typography variant="subtitle1" color="text.secondary" fontStyle="italic">
+        {errorMessage}
+      </Typography>
+    </Stack>
+  );
+}
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,18 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import Dashboard from "../pages/Dashboard";
+import ErrorPage from "../pages/ErrorPage";
+import Home from "../pages/Home";
 import PublicLayout from "../pages/layouts/PublicLayout";
 
 const router = createBrowserRouter([
   {
     element: <PublicLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
-        element: <h1>Home</h1>,
+        element: <Home />,
       },
       {
         path: "/dashboard",
-        element: <h1>Dashboard</h1>,
+        element: <Dashboard />,
       },
     ],
   },
