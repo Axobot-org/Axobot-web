be7d1f2dad567591471e004fc5790b86b3845388
ZRunner <z.runner.mc@gmail.com>
2023-07-12 15:42:29 -0400

    fix(auth): missing env variable in dev

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: "development" | "production" | "test";
     PUBLIC_URL: string;
+    REACT_APP_DISCORD_AUTH_REDIRECT: string;
     REACT_APP_API_URL: string;
     REACT_APP_DISCORD_CLIENT_ID: string;
   }
