95223115516c090d912c38ef74097cc2c7a8285a
ZRunner <z.runner.mc@gmail.com>
2024-03-09 16:29:09 -0500

    fix(vite): env variable loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr' 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [
-    react(),
-    svgr(),
-  ]
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.PUBLIC_URL,
+    server: {
+      port: env.PORT ? Number(env.PORT) : undefined,
+    },
+    plugins: [
+      react(),
+      svgr(),
+    ]
+  };
 })
