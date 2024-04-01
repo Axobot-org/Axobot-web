040590b535d3f769f29c4a33979c48ea86d08279
ZRunner <z.runner.mc@gmail.com>
2024-04-01 13:24:54 -0400

    feat(logout): reset states upon logout

--- src/repository/redux/dispatchs/useLogout.ts
+++ src/repository/redux/dispatchs/useLogout.ts
@@ -1,11 +1,15 @@
 import { useAppDispatch } from "../hooks";
 import { logout } from "../slices/userSlice";
+import { resetLeaderboards } from "../slices/leaderboardSlice";
+import { resetGuilds } from "../slices/guildSlice";
 
 export default function useLogout() {
   const dispatch = useAppDispatch();
 
   async function logoutCommand() {
     dispatch(logout());
+    dispatch(resetLeaderboards());
+    dispatch(resetGuilds());
     if (window._mtm) {
       window._mtm.push({ event: "logout", userId: undefined });
     }
