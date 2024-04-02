94c9938cff524f4679518b06feb7ce4b0eb44884
ZRunner <z.runner.mc@gmail.com>
2024-04-02 13:44:43 -0400

    refact(lint): fix some files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line unused-imports/no-unused-vars
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
