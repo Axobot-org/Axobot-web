a4b4db2dc5a9c8e4d9aa42e676530497e25950b3
ZRunner <z.runner.mc@gmail.com>
2024-04-01 23:05:35 -0400

    refact(leaderboard): use rtk API (WIP)

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -1,91 +1,86 @@
 import { Typography } from "@mui/material";
-import { Fragment, useEffect, useMemo, useState } from "react";
+import { Fragment, useMemo, useState } from "react";
+import { Helmet } from "react-helmet-async";
 import { Navigate, useParams } from "react-router-dom";
 
 import GlobalHeader from "../components/Leaderboard/GlobalHeader";
 import GuildHeader from "../components/Leaderboard/GuildHeader";
 import PlayersList from "../components/Leaderboard/PlayersList";
-import { useGetLeaderboard } from "../repository/commands/useGetLeaderboard";
-import { PLAYERS_PER_PAGE } from "../repository/redux/slices/leaderboardSlice";
-import NeedsLoginErrorPage from "./genericPages/NeedsLoginErrorPage";
-import { Helmet } from "react-helmet-async";
+import { useFetchLeaderboardQuery } from "../repository/redux/api";
 
-enum ErrorMessage {
-  InvalidToken = "Invalid token",
-  XpNotEnabled = "XP is not enabled for this guild",
-  UserNotMember = "User is not a member of this guild",
-  GuildNotFound = "Guild not found",
-  UserNotAuthenticated = "No authentication token found in request headers",
-}
+const PLAYERS_PER_PAGE = 30;
 
-function getErrorText(error: string) {
-  switch (error) {
-    case ErrorMessage.InvalidToken:
-      return "Authentication failed, please refresh the page and try again.";
-    case ErrorMessage.XpNotEnabled:
-      return "XP is not enabled for this guild.";
-    case ErrorMessage.UserNotMember:
-      return "You do not have permission to view this guild's leaderboard.";
-    case ErrorMessage.GuildNotFound:
-      return "This guild does not exist.";
-    default:
-      return "Sorry, an unexpected error has occurred.";
-  }
-}
+// enum ErrorMessage {
+//   InvalidToken = "Invalid token",
+//   XpNotEnabled = "XP is not enabled for this guild",
+//   UserNotMember = "User is not a member of this guild",
+//   GuildNotFound = "Guild not found",
+//   UserNotAuthenticated = "No authentication token found in request headers",
+// }
+
+// function getErrorText(error: string) {
+//   switch (error) {
+//   case ErrorMessage.InvalidToken:
+//     return "Authentication failed, please refresh the page and try again.";
+//   case ErrorMessage.XpNotEnabled:
+//     return "XP is not enabled for this guild.";
+//   case ErrorMessage.UserNotMember:
+//     return "You do not have permission to view this guild's leaderboard.";
+//   case ErrorMessage.GuildNotFound:
+//     return "This guild does not exist.";
+//   default:
+//     return "Sorry, an unexpected error has occurred.";
+//   }
+// }
 
 const LeaderboardPage = ({ guildId }: { guildId: string }) => {
-  const { fetchLeaderboardPage, leaderboard, error, loading } = useGetLeaderboard(guildId);
-  const [page, setPage] = useState(0);
-  const [lastRequestedPage, setLastRequestedPage] = useState(-1);
+  const [requestedPage, setRequestedPage] = useState(0);
+  const { data, error, isLoading } = useFetchLeaderboardQuery({ guildId, page: requestedPage, limit: PLAYERS_PER_PAGE });
 
-  useEffect(() => {
-    const lastLoadedPage = Math.ceil(Object.keys(leaderboard ?? {}).length / PLAYERS_PER_PAGE) - 1;
-    if (lastLoadedPage < page && lastRequestedPage < page && !loading && !error) {
-      console.debug("fetching page", page);
-      setLastRequestedPage(page);
-      fetchLeaderboardPage(page);
-    }
-  }, [error, fetchLeaderboardPage, page, leaderboard, loading, lastRequestedPage]);
+  const loading = isLoading || (guildId === "global" && !!data?.guild) || (guildId !== "global" && data?.guild?.id !== guildId);
+  const leaderboard = loading ? undefined : data;
 
   const players = useMemo(() => {
-    if (leaderboard === null) {
+    if (leaderboard === undefined) {
       return [];
     }
 
     return Object.entries(leaderboard.players)
       .sort((a, b) => a[1].ranking - b[1].ranking)
-      .map(([id, points]) => points);
+      .map(([_id, points]) => points);
   }, [leaderboard]);
 
   const guildData = useMemo(() => {
     if (guildId === "global") {
       return null;
     }
-    if (leaderboard?.guildData) {
-      return leaderboard.guildData;
+    if (leaderboard?.guild) {
+      return leaderboard.guild;
     }
     return {
       id: guildId,
       name: "Loading...",
       icon: null,
     };
-  }, [guildId, leaderboard?.guildData]);
+  }, [guildId, leaderboard?.guild]);
 
-  const hasNextPage = leaderboard?.totalCount ? leaderboard.totalCount > (page + 1) * PLAYERS_PER_PAGE : false;
+  const hasNextPage = leaderboard?.totalCount ? leaderboard.totalCount > (requestedPage + 1) * PLAYERS_PER_PAGE : false;
 
-  // check if the guild is in private mode and user is not registered
-  if (error === ErrorMessage.UserNotAuthenticated) {
-    return <NeedsLoginErrorPage />;
-  }
+  // TODO: check if the guild is in private mode and user is not registered
+  // if (error === ErrorMessage.UserNotAuthenticated) {
+  //   return <NeedsLoginErrorPage />;
+  // }
 
   function loadMore() {
-    if (!hasNextPage) {
+    const lastLoadedPage = Math.ceil(Object.keys(leaderboard?.players ?? {}).length / PLAYERS_PER_PAGE) - 1;
+    if (!hasNextPage || loading || error) {
       return;
     }
-    setPage(page + 1);
+    console.debug("fetching page", lastLoadedPage + 1);
+    setRequestedPage(lastLoadedPage + 1);
   }
 
-  if (error !== null) {
+  if (error !== undefined) {
     console.error(error);
     return (
       <Fragment>
@@ -94,7 +89,7 @@ const LeaderboardPage = ({ guildId }: { guildId: string }) => {
           Oops, something went wrong!
         </Typography>
         <Typography variant="subtitle1" color="text.secondary" fontStyle="italic">
-          {getErrorText(error)}
+          {`${error}`}
         </Typography>
       </Fragment>
     );
@@ -114,8 +109,8 @@ const MetaTags = ({ guildId }: { guildId: string }) => {
     <Helmet>
       <title>Axobot: {pageTitle}</title>
     </Helmet>
-  )
-}
+  );
+};
 
 export default function Leaderboard() {
   const { id } = useParams();
