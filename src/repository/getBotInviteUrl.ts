8866d9649652c6f2a2cb13c7560db07d8899b5c3
ZRunner <z.runner.mc@gmail.com>
2024-04-03 19:03:51 -0400

    feat(GuildBox): add invite button on disabled guilds

--- src/repository/getBotInviteUrl.ts
+++ src/repository/getBotInviteUrl.ts
@@ -1,7 +1,10 @@
-export default function getBotInviteUrl() {
+export default function getBotInviteUrl(guildId: string | undefined = undefined) {
   const params = new URLSearchParams({
     "client_id": import.meta.env.VITE_DISCORD_CLIENT_ID,
     "scope": "bot",
   });
+  if (guildId) {
+    params.set("guild_id", guildId);
+  }
   return "https://discord.com/api/oauth2/authorize?" + params;
 }
