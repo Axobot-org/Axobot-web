65c722fd5983040c339a0b976ae1d6aa829c52f0
ZRunner <z.runner.mc@gmail.com>
2024-04-22 16:49:15 -0400

    fix(ssr): remove broken hydration thing

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -4,11 +4,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.hydrateRoot(
+const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
