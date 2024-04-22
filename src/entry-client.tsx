63746ceee06e07d86128fec189d751a6bc372f99
ZRunner <z.runner.mc@gmail.com>
2024-04-22 16:32:37 -0400

    feat(client): add back web vitals

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.hydrateRoot(
   document.getElementById("root") as HTMLElement,
@@ -9,3 +10,8 @@ ReactDOM.hydrateRoot(
     <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
