4cf2e0f5e289f5ec82cd193d8647b2e04a7d0f35
ZRunner <z.runner.mc@gmail.com>
2024-04-23 16:33:32 -0400

    fix(useGetorFetchMe): avoid query if no token is found

--- src/repository/commands/useGetOrFetchMe.ts
+++ src/repository/commands/useGetOrFetchMe.ts
@@ -1,6 +1,8 @@
 import { useFetchMeQuery } from "../redux/api/api";
+import useTokenSelector from "../redux/tokenStorage/useTokenSelector";
 
 export function useGetorFetchMe() {
-  const { data, error, isLoading } = useFetchMeQuery();
+  const token = useTokenSelector();
+  const { data, error, isLoading } = useFetchMeQuery(undefined, { skip: token === null });
   return { user: data, error, loading: isLoading };
 }
