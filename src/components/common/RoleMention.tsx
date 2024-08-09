c28f152e1a153fd6d4bc1181c2c4c9e999384df4
ZRunner <z.runner.mc@gmail.com>
2024-08-09 14:12:01 +0200

    fix(leaderboard): role mention display

--- src/components/common/RoleMention.tsx
+++ src/components/common/RoleMention.tsx
@@ -14,14 +14,19 @@ export default function RoleMention({ name, color, onDelete }: RoleMentionProps)
   const backgroundColorAsRGBA = `rgba(${(mentionColor >> 16) & 255}, ${(mentionColor >> 8) & 255}, ${mentionColor & 255}, 0.1)`;
 
   return (
-    <Stack direction="row" gap={0.5} sx={{
-      color: mentionColorAsHex,
-      backgroundColor: backgroundColorAsRGBA,
-      borderRadius: "5px",
-      alignItems: "center",
-      padding: onDelete ? "2px 4px" : "0 2px",
-      mx: onDelete ? 0.5 : 0,
-    }}>
+    <Stack
+      display="inline-flex"
+      direction="row"
+      gap={0.5}
+      sx={{
+        color: mentionColorAsHex,
+        backgroundColor: backgroundColorAsRGBA,
+        borderRadius: "5px",
+        alignItems: "center",
+        padding: onDelete ? "2px 4px" : "0 2px",
+        mx: onDelete ? 0.5 : 0,
+      }}
+    >
       @{name}
       {onDelete && <DeleteCircleButton onClick={onDelete} />}
     </Stack>
