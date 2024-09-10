e731128713321a5996f7bc9655abc383031fc490
ZRunner <z.runner.mc@gmail.com>
2024-09-10 18:00:33 +0200

    feat(dashboard): better error message for missing requirements

--- src/components/GuildDashboard/ConfigComponents/shared/SharedConfigComponents.tsx
+++ src/components/GuildDashboard/ConfigComponents/shared/SharedConfigComponents.tsx
@@ -1,10 +1,10 @@
-import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import PriorityHighIcon from "@mui/icons-material/PriorityHigh";
 import SyncProblemIcon from "@mui/icons-material/SyncProblem";
 import { Stack, styled, Tooltip, Typography } from "@mui/material";
 import { PropsWithChildren } from "react";
 
 import { getGuildDashboardTranslations } from "../../../../i18n/i18n";
-import { useConfigComponentContext } from "../../../../repository/context/ConfigComponentContext";
+import { getMissingOptionRequirement, useConfigComponentContext } from "../../../../repository/context/ConfigComponentContext";
 import useIsConfigEdited from "./useIsConfigEdited";
 
 interface SimpleConfigurationProps {
@@ -93,14 +93,29 @@ function EditedBadge() {
 }
 
 function MissingRequirementLabel() {
-  const { isDisabled } = useConfigComponentContext();
+  const { option, isDisabled, config } = useConfigComponentContext();
   if (!isDisabled) return null;
+  const missingRequirement = getMissingOptionRequirement(option, config);
+  if (missingRequirement === null) return null;
 
-  const message = "Missign requirements";
+  const optionName = getGuildDashboardTranslations("option_name." + missingRequirement.option);
+  let message = `Missign requirement: '${optionName}'`;
+  if ("to_be_defined" in missingRequirement) {
+    message = `Requires '${optionName}' to be defined`;
+  } else if ("to_be" in missingRequirement) {
+    if (Array.isArray(missingRequirement.to_be)) {
+      message = `Requires '${optionName}' to be one of: ${missingRequirement.to_be.join(", ")}`;
+    } else if (typeof missingRequirement.to_be === "boolean") {
+      message = `Requires '${optionName}' to be ${missingRequirement.to_be ? "enabled" : "disabled"}`;
+    } else {
+      message = `Requires '${optionName}' to be '${missingRequirement.to_be}'`;
+    }
+  }
+
   return (
-    <Stack direction="row" gap={0.5} component="span" display="inline-flex" sx={{ verticalAlign: "sub" }}>
-      <ErrorOutlineIcon color="error" fontSize="small" />
-      <Typography component="span" variant="caption" color="error" sx={{ display: "inline-block" }}>{message}</Typography>
+    <Stack direction="row"component="span" display="inline-flex" sx={{ verticalAlign: "sub" }}>
+      <PriorityHighIcon color="error" fontSize="small" />
+      <Typography component="span" variant="caption" color="text.secondary" sx={{ display: "inline-block" }}>{message}</Typography>
     </Stack>
   );
 }
