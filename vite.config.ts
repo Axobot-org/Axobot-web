d90fc68ef0fc20d69d4edd538ba153bdef8c18ff
ZRunner <z.runner.mc@gmail.com>
2026-01-03 17:30:57 +0100

    refact(config): type-checking for env and rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { reactRouter } from "@react-router/dev/vite";
+import assert from "assert";
 import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, isSsrBuild }) => {
   const env = loadEnv(mode, process.cwd(), "");
+
+  assert(!!env.PUBLIC_URL, "PUBLIC_URL must be defined in the environment variables.");
 
   const publicUrl = env.PUBLIC_URL.endsWith("/") ? env.PUBLIC_URL : env.PUBLIC_URL + "/";
 
@@ -15,11 +18,6 @@ export default defineConfig(({ mode, isSsrBuild }) => {
     },
     build: {
       outDir: "build",
-      rollupOptions: isSsrBuild
-        ? {
-          input: ["virtual:react-router/server-build"],
-        }
-        : undefined,
     },
     plugins: [
       reactRouter(),
