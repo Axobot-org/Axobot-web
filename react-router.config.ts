07206e707c2d17ba7c3ca0cfa06602d417887bc5
ZRunner <z.runner.mc@gmail.com>
2026-01-03 17:36:30 +0100

    fix(lint): missing space

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   appDirectory: "src",
   ssr: false,
-  prerender:["/", "/terms", "/privacy"],
+  prerender: ["/", "/terms", "/privacy"],
 } satisfies Config;
