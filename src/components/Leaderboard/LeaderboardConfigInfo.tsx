61d85fffff5bcd85e3226aeb77958373448f7ebe
ZRunner <z.runner.mc@gmail.com>
2026-06-20 13:38:35 +0200

    fix(leaderboard): config info formatting and spacing

--- src/components/Leaderboard/LeaderboardConfigInfo.tsx
+++ src/components/Leaderboard/LeaderboardConfigInfo.tsx
@@ -12,7 +12,12 @@ interface LeaderboardConfigInfoProps {
   roleRewards: RoleReward[] | undefined;
 }
 
-export default function LeaderboardConfigInfo({ xpType, xpRate, xpDecay, roleRewards }: LeaderboardConfigInfoProps) {
+export default function LeaderboardConfigInfo({
+  xpType,
+  xpRate,
+  xpDecay,
+  roleRewards,
+}: LeaderboardConfigInfoProps) {
   const useLocalLeaderboard = xpType !== "global";
   const hasRoleRewards = roleRewards && roleRewards.length > 0;
   return (
@@ -58,72 +63,63 @@ function XpTypeInfo({ xpType }: { xpType: LeaderboardConfigInfoProps["xpType"] }
     case "global":
       return (
         <>
-          This server uses the
-          <b>global leaderboard</b>
-          . Members can get XP from any other server that also uses the global leaderboard.
+          This server uses the <b>global leaderboard</b>. Members can get XP from any other server that also
+          uses the global leaderboard.
         </>
       );
     case "local":
       return (
         <>
-          This server uses the
-          <b>local leaderboard</b>
-          . Members can only get XP from this server, based on their chat activity.
+          This server uses the <b>local leaderboard</b>. Members can only get XP from this server, based on
+          their chat activity.
         </>
       );
     case "mee6-like":
       return (
         <>
-          This server use the
-          <b>mee6-like leaderboard</b>
-          . Members can only get their XP from this server, based on the number of messages they send.
+          This server uses the <b>mee6-like leaderboard</b>. Members can only get their XP from this server,
+          based on the number of messages they send.
         </>
       );
     default:
       return (
         <>
-          This server use the
-          <b>{xpType}</b>
-          {" "}
-          leaderboard.
+          This server uses the <b>{xpType}</b> leaderboard.
         </>
       );
   }
 }
 
-function XpRateInfo({ xpType, xpRate }: { xpType: LeaderboardConfigInfoProps["xpType"]; xpRate: LeaderboardConfigInfoProps["xpRate"] }) {
+function XpRateInfo({
+  xpType,
+  xpRate,
+}: {
+  xpType: LeaderboardConfigInfoProps["xpType"];
+  xpRate: LeaderboardConfigInfoProps["xpRate"];
+}) {
   const formatedRate = formatNumber(xpRate);
   if (xpType === "mee6-like") {
     const minValue = formatNumber(xpRate * 15);
     const maxValue = formatNumber(xpRate * 25);
     return (
       <>
-        The XP rate is
-        <b>x{formatedRate}</b>
-        : each message sent will randomly reward between
-        {minValue}
-        {" "}
-        and
-        {maxValue}
-        {" "}
-        XP.
+        The XP rate is <b>x{formatedRate}</b>: each message sent will randomly reward between {minValue} and{" "}
+        {maxValue} XP.
       </>
     );
   } else if (xpType === "local") {
     if (xpRate > 1) {
       return (
         <>
-          The XP rate is
-          <b>x{formatedRate}</b>
-          : each message will reward more XP than the default value, based on its length.
+          The XP rate is <b>x{formatedRate}</b>: each message will reward more XP than the default value,
+          based on its length.
         </>
       );
     } else {
       return (
         <>
-          The XP rate is
-          <b>x{formatedRate}</b>
-          : each message will reward less XP than the default value, based on its length
+          The XP rate is <b>x{formatedRate}</b>: each message will reward less XP than the default value,
+          based on its length.
         </>
       );
     }
@@ -136,8 +132,7 @@ function XpDecayInfo({ xpDecay }: { xpDecay: LeaderboardConfigInfoProps["xpDecay
     return (
       <>
         The XP decay is
-        <b>disabled</b>
-        : members will not lose XP over time.
+        <b>disabled</b>: members will not lose XP over time.
       </>
     );
   }
@@ -145,30 +140,24 @@ function XpDecayInfo({ xpDecay }: { xpDecay: LeaderboardConfigInfoProps["xpDecay
   return (
     <>
       The XP decay is set to
-      <b>{formatedValue} XP</b>
-      : each member will lose
-      {formatedValue}
-      {" "}
-      XP every day, no matter their activity.
+      <b>{formatedValue} XP</b>: each member will lose
+      {formatedValue} XP every day, no matter their activity.
     </>
   );
 }
 
 function RoleRewardsInfo({ roleRewards }: { roleRewards: RoleReward[] }) {
-  const sortedRoleRewars = roleRewards.toSorted((a, b) => ((a.level < b.level) ? -1 : ((a.level > b.level) ? 1 : 0)));
+  const sortedRoleRewars = roleRewards.toSorted((a, b) =>
+    a.level < b.level ? -1 : a.level > b.level ? 1 : 0,
+  );
   return (
     <>
-      This server uses the following
-      {" "}
-      <b>role rewards</b>
-      :
+      This server uses the following <b>role rewards</b>:
       <List dense sx={{ listStyleType: "circle", py: 0 }}>
         {sortedRoleRewars.map((roleReward) => (
           <ListItem key={roleReward.roleId} sx={{ py: 0.2 }}>
-            <RoleMention name={roleReward.role?.name ?? roleReward.roleId} color={roleReward.role?.color} />
-            {" "}
-            at level
-            {formatNumber(roleReward.level)}
+            <RoleMention name={roleReward.role?.name ?? roleReward.roleId} color={roleReward.role?.color} />{" "}
+            at level {formatNumber(roleReward.level)}
           </ListItem>
         ))}
       </List>
