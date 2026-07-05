c6bbf7d99fc3ec8e8a167714c9310c6cee788c66
ZRunner <z.runner.mc@gmail.com>
2026-07-05 13:42:00 +0200

    fix(build): remove v8 futures config

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,11 +4,4 @@ export default {
   appDirectory: "src",
   ssr: false,
   prerender: ["/", "/terms", "/privacy"],
-  future: {
-    v8_middleware: true,
-    v8_splitRouteModules: true,
-    v8_viteEnvironmentApi: true,
-    v8_passThroughRequests: true,
-    v8_trailingSlashAwareDataRequests: true,
-  },
 } satisfies Config;
